7cb892f2a8fad24246069067e0ee52d4b2a49ebe
halima04 <115195110+halima04@users.noreply.github.com>
2022-10-25 19:26:53 -0400

    Add files via upload

---
+++ Getting-Started-with-Python/about_me.py
@@ -0,0 +1,14 @@
+full_name = "Halima Aktar"
+age = 17
+has_used_python = False 
+
+hobbies = ["knitting","drawing","volleyball"]
+
+fav_things = {
+  "movie": "Maleficent",
+  "food": "somosas",
+  "hobby": hobbies[0],
+  "number": 5
+}
+
+all_vars = dict(vars())
